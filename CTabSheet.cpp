91815d0a3ad37e52069b56f887f72f41316e3870
Xiaomony <117056212+Xiaomony@users.noreply.github.com>
2022-11-08 09:12:26 +0800

    Add files via upload

---
+++ CTabSheet.cpp
@@ -0,0 +1,171 @@
+#include"pch.h"
+#include"CTabSheet.h"
+IMPLEMENT_DYNAMIC(CTabSheet, CTabCtrl)
+
+BEGIN_MESSAGE_MAP(CTabSheet,CTabCtrl)
+ON_NOTIFY_REFLECT(TCN_SELCHANGE, OnTcnSelchange)
+END_MESSAGE_MAP()
+
+CTabSheet::CTabSheet()
+{
+	current_page = -1;
+	pagex = 0;
+	pagey = 20;
+}
+
+CTabSheet::~CTabSheet()
+{
+}
+
+void CTabSheet::OnTcnSelchange(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	CRect rec;
+	GetClientRect(&rec);
+	int a=GetCurFocus();
+	pages[current_page].pageItem->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_HIDEWINDOW);
+	pages[a].pageItem->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_SHOWWINDOW);
+	current_page = a;
+}
+
+BOOL CTabSheet::AddPage(int pagenum, LPCTSTR pagename, UINT pageID,CDialog *pPage, BOOL whether_show_now)
+{
+	if (pPage == NULL) return NULL_PAGE;
+	if (pagenum > pages.size()) return ADD_OUT;
+	page p;
+	p.pageItem = pPage;
+	p.ID = pageID;
+	p.title = pagename;
+	int a = pages.size();
+	if (pagenum == pages.size())
+		pages.push_back(p);
+	else
+		pages.insert(pages.begin()+pagenum,p);
+
+
+	CRect rec;
+	GetClientRect(&rec);
+	InsertItem(pagenum, pagename);
+	BOOL b;
+	b=pPage->Create(pageID, this);
+	if (b == FALSE) return FALSE;
+	if (current_page == -1)
+	{
+		b = pPage->SetWindowPos(NULL,pagex,pagey, rec.Width(), rec.Height(), SWP_SHOWWINDOW);
+		current_page = pagenum;
+		if (whether_show_now == FALSE) return NO_CURRENT_PAGE;
+		return b;
+	}
+	if (whether_show_now)
+	{
+		pages[current_page].pageItem->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_HIDEWINDOW);
+		b = pPage->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_SHOWWINDOW);
+		SetCurFocus(pagenum);
+	}
+	else
+		b=pPage->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_HIDEWINDOW);
+	return b;
+}
+
+BOOL CTabSheet::AddPageInEnd(LPCTSTR pagename, UINT pageID, CDialog* pPage, BOOL whether_show_now)
+{
+	//int pagenum = pages.size();
+	BOOL b =AddPage(pages.size(), pagename, pageID, pPage, whether_show_now);
+	return b;
+}
+
+
+
+int CTabSheet::GetCurrentPage()
+{
+	if (current_page == -1)
+		return NO_CURRENT_PAGE;
+	return current_page;
+}
+
+
+BOOL CTabSheet::GetCurrentPage(page& page_struct)
+{
+	if (current_page == -1)
+		return NO_CURRENT_PAGE;
+	page_struct = pages[current_page];
+}
+
+BOOL CTabSheet::GetCurrentPage(int& pagenum, page& page_struct)
+{
+	if (current_page == -1)
+		return NO_CURRENT_PAGE;
+	pagenum = current_page;
+	page_struct = pages[current_page];
+}
+
+BOOL CTabSheet::DeletePage(CString name)
+{
+	for (int i = 0; i < pages.size(); i++)
+	{
+		if (pages[i].title == name)
+		{
+			DeletePage(i);
+			return TRUE;
+		}
+	}
+	return DELETE_NOT_FOUND;
+}
+
+BOOL CTabSheet::DeletePage(int pagenum)
+{
+	if (pagenum > pages.size() - 1)
+		return DELETE_OUT;
+	CRect rec;
+	GetClientRect(&rec);
+	pages[pagenum].pageItem->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_HIDEWINDOW);
+	DeleteItem(pagenum);
+	pages.erase(pages.begin()+pagenum);
+	if (pages.size() == 0)
+	{
+		current_page = -1;
+		return TRUE;
+	}
+	if (current_page==pagenum)
+	{
+		if (current_page == 0)
+			pages[pagenum].pageItem->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_SHOWWINDOW);
+		else
+		{
+			current_page = pagenum - 1;
+			pages[pagenum - 1].pageItem->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_SHOWWINDOW);
+		}
+	}
+	else if (current_page>pagenum)
+	{
+		current_page--;
+		pages[current_page].pageItem->SetWindowPos(NULL, pagex, pagey, rec.Width(), rec.Height(), SWP_SHOWWINDOW);
+	}
+	return TRUE;
+}
+
+BOOL CTabSheet::DeleteCurrentPage()
+{
+	if (current_page == -1)
+		return NO_CURRENT_PAGE;
+	DeletePage(current_page);
+	return TRUE;
+}
+
+int CTabSheet::FindPage(CString pagename)
+{
+	for (int i = 0; i < pages.size(); i++)
+		if (pages[i].title == pagename)
+			return i;
+	return -1;
+}
+
+BOOL CTabSheet::FindPage(CString pagename, page &p)
+{
+	for (int i = 0; i < pages.size(); i++)
+		if (pages[i].title == pagename)
+		{
+			p = pages[i];
+			return TRUE;
+		}
+	return FALSE;
+}
---
+++ CTabSheet.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <afxwin.h>
+#include <afxext.h>
+#include <afxcmn.h>
+#include <vector>
+
+#define ADD_OUT 2			//添加的索引溢出
+#define NO_CURRENT_PAGE 3	//当前未展示页面(若在AddPage()中，则强制展示插入的页面)
+#define DELETE_OUT 4
+#define DELETE_NOT_FOUND 5
+#define NULL_PAGE 6
+
+struct page {
+	CDialog* pageItem;
+	UINT ID;
+	CString title;
+};
+class CTabSheet :public CTabCtrl
+{
+	DECLARE_DYNAMIC(CTabSheet)
+	DECLARE_MESSAGE_MAP()
+protected:
+	//当前页面的索引
+	int current_page;
+	//存储所有的页面
+	std::vector<page> pages;
+	//页面切换
+	afx_msg void OnTcnSelchange(NMHDR* pNMHDR, LRESULT* pResult);
+	//页面显示时的左上角横坐标（相对与tab控件）
+	int pagex;
+	//页面显示时的左上角纵坐标（相对与tab控件）
+	int pagey;
+public:
+	CTabSheet();
+	virtual ~CTabSheet();
+	//根据索引添加页面
+	BOOL AddPage(int pagenum, LPCTSTR pagename ,UINT pageID, CDialog* pPage, BOOL whether_show_now);
+	//在最后添加页面
+	BOOL AddPageInEnd(LPCTSTR pagename, UINT pageID, CDialog* pPage, BOOL whether_show_now);
+	//返回当前页面的索引
+	int GetCurrentPage();
+	//返回当前页面的page结构体
+	BOOL GetCurrentPage(page &page_struct);
+	//返回当前页面的索引和结构体
+	BOOL GetCurrentPage(int& pagenum, page& page_struct);
+	//根据页面名称删除页面
+	BOOL DeletePage(CString name);
+	//根据索引删除页面
+	BOOL DeletePage(int pagenum);
+	//删除当前页面
+	BOOL DeleteCurrentPage();
+	//通过名称获取页面索引
+	int FindPage(CString pagename);
+	//通过名称获取页面page结构体
+	BOOL FindPage(CString pagename,page &p);
+};
